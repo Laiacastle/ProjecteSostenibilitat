cf5af1ecbd4c0c9843d380478df6f94e1aa7402f
lluc.velazquez.7e8 <lluc.velazquez.7e8@itb.cat>
2025-05-19 12:28:48 +0200

    Main Page Created

---
+++ AppDietes/composeApp/src/commonMain/kotlin/org/project/dietes/homePage/HomePageScreen.kt
@@ -0,0 +1,8 @@
+package org.project.dietes.homePage
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomePageScreen(navigateToGamesScreen: () -> Unit, ) {
+
+}
---
+++ AppDietes/composeApp/src/commonMain/kotlin/org/project/dietes/homePage/HomePageViewModel.kt
@@ -0,0 +1,7 @@
+package org.project.dietes.homePage
+
+import androidx.lifecycle.ViewModel
+
+class HomePageViewModel: ViewModel() {
+
+}
