0fdb2c901fe1e11ebb2aaff8f12984ab3c6b226a
lluc.velazquez.7e8 <lluc.velazquez.7e8@itb.cat>
2025-05-20 12:39:30 +0200

    User Statistics Screen Validate TextField

--- AppDietes/composeApp/src/commonMain/kotlin/org/project/dietes/UserStatisticsScreen.kt
+++ AppDietes/composeApp/src/commonMain/kotlin/org/project/dietes/UserStatisticsScreen.kt
@@ -11,7 +11,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.ContentAlpha
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material3.Button
+import androidx.compose.material.Button
 import androidx.compose.material3.Icon
 //noinspection UsingMaterialAndMaterial3Libraries
 import androidx.compose.material.OutlinedTextField
@@ -49,21 +49,29 @@ class UserDataViewModel : ViewModel(){
 @Composable
 fun UserStatisticsScreen(){
     val users = UserDataViewModel().users
-    var name by remember { mutableStateOf(TextFieldValue("")) }
-    var nameError by remember { mutableStateOf(false) } // 1
-    var lastName by remember { mutableStateOf(TextFieldValue("")) }
-    var email by remember { mutableStateOf(TextFieldValue("")) }
-    var weight by remember { mutableStateOf(TextFieldValue("")) }
-    var exerciseDone by remember { mutableStateOf(TextFieldValue("")) }
-    var sleepTime by remember { mutableStateOf(TextFieldValue("")) }
-    var age by remember { mutableStateOf(TextFieldValue("")) }
+    var name by remember { mutableStateOf("") }
+    var nameError by remember { mutableStateOf(false) }
+    var lastName by remember { mutableStateOf("") }
+    var lastNameError by remember { mutableStateOf(false) }
+    var email by remember { mutableStateOf("") }
+    var emailError by remember { mutableStateOf(false) }
+    var weight by remember { mutableStateOf("") }
+    var weightError by remember { mutableStateOf(false) }
+    var exerciseDone by remember { mutableStateOf("") }
+    var exerciseDoneError by remember { mutableStateOf(false) }
+    var sleepTime by remember { mutableStateOf("") }
+    var sleepTimeError by remember { mutableStateOf(false) }
+    var age by remember { mutableStateOf("") }
+    var ageError by remember { mutableStateOf(false) }
 
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ){
+
         Spacer(Modifier.height(10.dp))
         Text("Information User", fontSize = 20.sp)
+
         // name input
         OutlinedTextField(
             value = name,
@@ -76,31 +84,46 @@ fun UserStatisticsScreen(){
             isError = nameError,
             placeholder = {Text(text = "Enter your name")}
         )
-
-        val assistiveElementText = if (nameError) "Error: Obligatorio" else "*Obligatorio" // 4
-        val assistiveElementColor = if (nameError) { // 5
+        val assistiveElementText = if (nameError) "Error: Obligatorio" else "*Obligatorio"
+        val assistiveElementColor = if (nameError) {
             MaterialTheme.colors.error
         } else {
             MaterialTheme.colors.onSurface.copy(alpha = ContentAlpha.medium)
         }
-        Text(// 6
+        Text(
             text = assistiveElementText,
             color = assistiveElementColor,
             style = MaterialTheme.typography.caption,
-            modifier = Modifier.padding(start = 16.dp)
         )
+
         Spacer(Modifier.height(20.dp))
+
         // lastname input
         OutlinedTextField(
             value = lastName,
             onValueChange = {
                 lastName = it
+                lastNameError = false
             },
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
             label = {Text(text = "Last Name")},
+            isError = lastNameError,
             placeholder = {Text(text = "Enter your Last Name")}
         )
+        val assistiveElementTextLastName = if (lastNameError) "Error: Obligatorio" else "*Obligatorio"
+        val assistiveElementColorLastName = if (lastNameError) {
+            MaterialTheme.colors.error
+        } else {
+            MaterialTheme.colors.onSurface.copy(alpha = ContentAlpha.medium)
+        }
+        Text(
+            text = assistiveElementTextLastName,
+            color = assistiveElementColorLastName,
+            style = MaterialTheme.typography.caption,
+        )
+
         Spacer(Modifier.height(20.dp))
+
         // email input
         OutlinedTextField(
             value = email,
@@ -108,45 +131,105 @@ fun UserStatisticsScreen(){
                 contentDescription = null)},
             onValueChange = {
                 email = it
+                emailError = false
             },
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
             label = { Text(text = "Email address") },
+            isError = emailError,
             placeholder = { Text(text = "Enter your e-mail") }
         )
+        val assistiveElementTextEmail = if (emailError) "Error: Obligatorio" else "*Obligatorio"
+        val assistiveElementColorEmail = if (emailError) {
+            MaterialTheme.colors.error
+        } else {
+            MaterialTheme.colors.onSurface.copy(alpha = ContentAlpha.medium)
+        }
+        Text(
+            text = assistiveElementTextEmail,
+            color = assistiveElementColorEmail,
+            style = MaterialTheme.typography.caption,
+        )
+
         Spacer(Modifier.height(20.dp))
+
         // weight input
         OutlinedTextField(
             value = weight,
             onValueChange = {
                 weight = it
+                weightError = false
             },
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Decimal),
             label = {Text(text = "Weight")},
+            isError = weightError,
             placeholder = {Text(text = "Enter your Weight")}
         )
+        val assistiveElementTextWeight = if (weightError) "Error: Obligatorio" else "*Obligatorio"
+        val assistiveElementColorWeight = if (weightError) {
+            MaterialTheme.colors.error
+        } else {
+            MaterialTheme.colors.onSurface.copy(alpha = ContentAlpha.medium)
+        }
+        Text(
+            text = assistiveElementTextWeight,
+            color = assistiveElementColorWeight,
+            style = MaterialTheme.typography.caption,
+        )
+
         Spacer(Modifier.height(20.dp))
+
         // exerciseDone input
         OutlinedTextField(
             value = exerciseDone,
             onValueChange = {
                 exerciseDone = it
+                exerciseDoneError = false
             },
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
             label = {Text(text = "Exercise Done")},
+            isError = exerciseDoneError,
             placeholder = {Text(text = "Enter your Exercise Done")}
         )
+        val assistiveElementTextExercise = if (exerciseDoneError) "Error: Obligatorio" else "*Obligatorio"
+        val assistiveElementColorExercise = if (exerciseDoneError) {
+            MaterialTheme.colors.error
+        } else {
+            MaterialTheme.colors.onSurface.copy(alpha = ContentAlpha.medium)
+        }
+        Text(
+            text = assistiveElementTextExercise,
+            color = assistiveElementColorExercise,
+            style = MaterialTheme.typography.caption,
+        )
+
         Spacer(Modifier.height(20.dp))
+
         // sleepTime input
         OutlinedTextField(
             value = sleepTime,
             onValueChange = {
                 sleepTime = it
+                sleepTimeError = false
             },
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Decimal),
             label = {Text(text = "Sleep Time")},
+            isError = sleepTimeError,
             placeholder = {Text(text = "Enter your Sleep Time")}
         )
+        val assistiveElementTextSleep = if (sleepTimeError) "Error: Obligatorio" else "*Obligatorio"
+        val assistiveElementColorSleep = if (sleepTimeError) {
+            MaterialTheme.colors.error
+        } else {
+            MaterialTheme.colors.onSurface.copy(alpha = ContentAlpha.medium)
+        }
+        Text(
+            text = assistiveElementTextSleep,
+            color = assistiveElementColorSleep,
+            style = MaterialTheme.typography.caption,
+        )
+
         Spacer(Modifier.height(20.dp))
+
         // age input
         OutlinedTextField(
             value = age,
@@ -154,38 +237,65 @@ fun UserStatisticsScreen(){
                 contentDescription = null)},
             onValueChange = {
                 age = it
+                ageError = false
             },
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
             label = {Text(text = "Age")},
+            isError = ageError,
             placeholder = {Text(text = "Enter your Age")}
         )
+        val assistiveElementTextAge = if (ageError) "Error: Obligatorio" else "*Obligatorio"
+        val assistiveElementColorAge = if (ageError) {
+            MaterialTheme.colors.error
+        } else {
+            MaterialTheme.colors.onSurface.copy(alpha = ContentAlpha.medium)
+        }
+        Text(
+            text = assistiveElementTextAge,
+            color = assistiveElementColorAge,
+            style = MaterialTheme.typography.caption,
+        )
+
         Spacer(Modifier.height(20.dp))
+
         Button(
             onClick = {
-                //nameError = name.isBlank()
-                /*users.add( // no va
+                nameError = name.isBlank()
+                lastNameError = lastName.isBlank()
+                emailError = email.isBlank()
+                weightError = weight.isBlank()
+                exerciseDoneError = exerciseDone.isBlank()
+                sleepTimeError = sleepTime.isBlank()
+                ageError = age.isBlank()
+
+                // add user
+                users.add(
                     UserData(
                         idUser = 1, // generated Auto no fet
-                        name = name.toString(),
-                        lastName = lastName.toString(),
-                        email = email.toString(),
-                        weight = weight.toString().toFloat(),
-                        exerciseDone = exerciseDone.toString(),
-                        sleepTime = sleepTime.toString().toFloat(),
-                        age = age.toString().toInt(),
+                        name = name,
+                        lastName = lastName,
+                        email = email,
+                        weight = weight.toFloat(),
+                        exerciseDone = exerciseDone,
+                        sleepTime = sleepTime.toFloat(),
+                        age = age.toInt(),
                     )
-                )*/
-                // bbdd
+                )
             }
         ){
             Text("Envia")
         }
-        Text(text = name.toString())
         /*LazyColumn {
             items(users){ user ->
                 Row {
                     Column {
                         Text(user.name)
+                        Text(user.lastName)
+                        Text(user.email)
+                        Text(user.weight.toString())
+                        Text(user.exerciseDone)
+                        Text(user.sleepTime.toString())
+                        Text(user.age.toString())
                     }
                 }
             }
